b80f316053f0e3d2f4a208f5c98e5d9eb34acc55
ANKIT KUMAR <121149830+Ankit314@users.noreply.github.com>
2024-10-02 12:22:50 +0000

    variable

---
+++ basicjavascript/variable.js
@@ -0,0 +1,29 @@
+const name="Ankit";
+let number=9852263803;
+let address="Aurangabad";
+var pincode=824103;
+state="Bihar";
+
+
+console.log(name);
+console.log(number);
+console.log(address);
+console.log(pincode);
+console.log(state);
+console.table([name,number,address,pincode,state]);
+//name="kumar";
+number=9304957678;
+address="Negabigha";
+pincode=824101;
+state="Jharkhan"
+
+console.log(name);
+console.log(number);
+console.log(address);
+console.log(pincode);
+console.log(state);
+console.table([name,number,address,pincode,state]);
+
+
+
+
